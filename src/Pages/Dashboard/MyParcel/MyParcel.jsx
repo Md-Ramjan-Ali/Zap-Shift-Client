025ae6453a6aed5067303de6f374f3b7f452284d
ramjan88 <gmramjanali8888@gmail.com>
2025-07-06 02:03:49 +0600

    added my parcel

--- src/Pages/Dashboard/MyParcel/MyParcel.jsx
+++ src/Pages/Dashboard/MyParcel/MyParcel.jsx
@@ -1,9 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const MyParcel = () => {
+  const {user}=useAuth()
+  const axiosSecure=useAxiosSecure()
+
+  const {data: parcer=[]}=useQuery({
+    queryKey: ['myParcel',user.email],
+    queryFn: async()=>{
+      const res = await axiosSecure.get(`/parcels?email=${user.email}`);
+      return res.data
+    }
+  })
+
+  console.log(parcer);
   return (
     <div>
-      <h1>This is  my parcel</h1>
+      {/* <h1>This is  my parcel : {data?data.length:''}</h1> */}
+      <h1>This is  my parcel : {parcer.length}</h1>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router';
-import router from './Router/router.jsx';
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider } from "react-router";
+import router from "./Router/router.jsx";
 import "aos/dist/aos.css";
-import Aos from 'aos';
-import AuthProvider from './contexts/AuthContext/AuthProvider.jsx';
+import Aos from "aos";
+import AuthProvider from "./contexts/AuthContext/AuthProvider.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 Aos.init({
   duration: 1000,
   once: false,
 });
 
+const queryClient=new QueryClient()
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
